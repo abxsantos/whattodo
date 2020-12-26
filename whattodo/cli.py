92b1d9d28e138a1f44c6e7546bc222199b1af97d
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-12-26 11:26:06 -0300

    feat: added typer as dependency instead of click

--- whattodo/cli.py
+++ whattodo/cli.py
@@ -1,13 +1,55 @@
 """CLI for whattodo project."""
 
-import click
+import typer
+
+app = typer.Typer(help="WhatTodo CLI manager.")
+state = {"verbose": False}
 
 
-@click.group(context_settings={"help_option_names": ["-h", "--help"]})
-@click.version_option(version="0.0.0")
-def whattodo_cli():
-    """What Todo"""
+@app.command("board:add")
+def add_board(board_name: str):
+    """
+    Creates a new Board with BOARD_NAME to hold tasks.
+    """
+
+    if state["verbose"]:
+        typer.echo("About to create a board with name {board_name}")
+    typer.echo(f"Creating a board with name {board_name}")
+    if state["verbose"]:
+        typer.echo("Just created the board {board_name}")
+
+
+@app.command("task:add")
+def add_task(description: str):
+    """
+    Creates a new task with a DESCRIPTION to an active board.
+    """
+    if state["verbose"]:
+        typer.echo("About to add a new task to board")
+    typer.echo(
+        f"Creating a task with description {description} to board {state['board']}"
+    )
+    if state["verbose"]:
+        typer.echo("Just created the task {description}")
+
+
+@app.command("task:update")
+def update_task(status: str):
+    """
+    Updates task with to the given STATUS.
+    """
+    typer.echo(f"Updating the task to status {status}")
+
+
+@app.callback()
+def main(verbose: bool = False):
+    """
+    Manage Tasks in the awesome CLI app.
+    """
+    if verbose:
+        typer.echo("Will write verbose output")
+        state["verbose"] = True
 
 
 if __name__ == "__main__":
-    whattodo_cli()
+    app()
